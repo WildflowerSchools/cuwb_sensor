a22c739b2f8d6a409ad9c35937ace2677c89b251
Ben Talberg <ben.talberg@gmail.com>
2021-05-17 15:56:51 -0500

    Fix issue with paths when executing shell scripts

--- scripts/network_health.sh
+++ scripts/network_health.sh
@@ -5,4 +5,4 @@ export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export CUWB_NETWORK_NAME=$CUWB_NETWORK_NAME
 
-/usr/bin/python3 -m cuwb_sensor.tools check-network-health --name ${CUWB_NETWORK_NAME}
+PYTHONPATH=$PYTHONPATH:/usr/lib/wildflower/cuwb_sensor/ /usr/bin/python3 -m cuwb_sensor.tools check-network-health --name ${CUWB_NETWORK_NAME}
--- scripts/startnetwork.sh
+++ scripts/startnetwork.sh
@@ -3,4 +3,4 @@
 source /usr/lib/wildflower/cuwb_sensor/environment.env
 export CUWB_NETWORK_NAME=$CUWB_NETWORK_NAME
 
-/usr/bin/python3 -m cuwb_sensor.tools network --name ${CUWB_NETWORK_NAME} --action start
+PYTHONPATH=$PYTHONPATH:/usr/lib/wildflower/cuwb_sensor/ /usr/bin/python3 -m cuwb_sensor.tools network --name ${CUWB_NETWORK_NAME} --action start
