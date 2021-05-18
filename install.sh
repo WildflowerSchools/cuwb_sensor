90535f6fd1eddd290e8a65eef30d24e8ac6a494d
Ben Talberg <ben.talberg@gmail.com>
2021-05-17 21:41:50 -0500

    update install.sh to preserve executable permissions and to capture stderr when running

--- install.sh
+++ install.sh
@@ -18,9 +18,9 @@ pip3 install -r requirements.txt
 
 cp -r cuwb_sensor $APP_LOCATION
 cp -r environment.env $APP_LOCATION
-cp -r scripts/network_health.sh $APP_LOCATION
-cp -r scripts/startnetwork.sh $APP_LOCATION
-cp -r scripts/stopnetwork.sh $APP_LOCATION
+cp -rp scripts/network_health.sh $APP_LOCATION
+cp -rp scripts/startnetwork.sh $APP_LOCATION
+cp -rp scripts/stopnetwork.sh $APP_LOCATION
 
 echo "setting up logrotate for the logs"
 
@@ -50,9 +50,9 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 */10 * * * * root /usr/sbin/logrotate /etc/logrotate.conf
 
-0 4 * * *    root /usr/lib/wildflower/cuwb_sensor/stopnetwork.sh >> /data/scheduler.log
-5 4 * * *    root /usr/lib/wildflower/cuwb_sensor/startnetwork.sh >> /data/scheduler.log
-*/5 * * * *    root /usr/lib/wildflower/cuwb_sensor/network_health.sh >> /data/network_health.log
+0 4 * * *    root /usr/lib/wildflower/cuwb_sensor/stopnetwork.sh >> /data/scheduler.log 2>&1
+5 4 * * *    root /usr/lib/wildflower/cuwb_sensor/startnetwork.sh >> /data/scheduler.log 2>&1
+*/5 * * * *    root /usr/lib/wildflower/cuwb_sensor/network_health.sh >> /data/network_health.log 2>&1
 EOF
 
 systemctl restart cron.service
